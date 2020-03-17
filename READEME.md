68ac3b32d8c950909e5d2d535328ef899b6e5b2e
Jonathan Allen <nocjallen@protonmail.com>
2020-03-16 20:40:46 -0400

    Moving project to GitHub.

---
+++ READEME.md
@@ -0,0 +1,3 @@
+This project supports [Harrowing Echoes](https://github.com/nocjallen/harrowing-echoes).
+
+From a terminal, run `npm install` followed by `npm server.js`.
---
+++ server.js
@@ -0,0 +1,19 @@
+const restify = require ('restify');
+
+const server = restify.createServer();
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+})
+
+server.get('/maps', function (req, res) {
+  res.send(200, {
+    level: ["#########", "#•••••••#", "#•••@•••#", "#•••••••#", "#########"]
+  });
+})
+
+server.get('/scores', function (req, res) {
+  res.send(200, {
+    scores: ["Chad the Lvl 1 commoner, killed by a giant."]
+  })
+})
