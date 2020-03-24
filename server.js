65999462ac24935e465e54383fadc61380ac0b2c
Jonathan Allen <nocjallen@protonmail.com>
2020-03-24 01:34:57 -0400

    Resolved CORS browser errors by integrating the restify-cors-middleware package.

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+const corsMiddleware = require('restify-cors-middleware');
 const restify = require ('restify');
 
+const cors = corsMiddleware({
+  preflightMaxAge: 5, //Optional
+  origins: ['http://localhost:3000'],
+  allowHeaders: ['API-Token'],
+  exposeHeaders: ['API-Token-Expiry']
+})
+
 const server = restify.createServer();
+server.pre(cors.preflight)
+server.use(cors.actual)
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
@@ -19,7 +29,7 @@ server.get('/maps', function (req, res) {
           ['#','#','#','#','#','#','#','#','#']],
     title: "A Simple Map"
   }
-  });
+  })
 })
 
 server.get('/scores', function (req, res) {
